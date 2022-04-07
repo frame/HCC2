b3ae3346802e2c66413af8b99ca1f280f555fdea
Bernd Kilga <frame@console.cc>
2022-04-07 15:36:49 +0200

    renamed compile.bat, added compile-installer.bat

--- compile_client.bat
+++ compile_client.bat
@@ -2,4 +2,3 @@
 
 cd client
 "C:\Program Files\CodeBlocks\Codeblocks.exe" /na /nd /ns --build HCC.cbp --target="Win32 Release" > ..\Build\compile.log
-
---
+++ compile_installer.bat
@@ -0,0 +1,4 @@
+@echo off
+
+cd installer\nsis\
+"C:\Program Files (x86)\NSIS\makensis.exe" HCC_installer.nsi
