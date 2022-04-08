e296816e7c87c2b612ec6bbeffebf00bf601862d
Bernd Kilga <frame@console.cc>
2022-04-08 02:18:05 +0200

    overwrite existing database.zip

--- compile_database.bat
+++ compile_database.bat
@@ -1,3 +1,3 @@
 @echo off
 
-PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Compress-Archive -Path database\* -DestinationPath build\database.zip"
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "Compress-Archive -Path database\* -Force -DestinationPath build\database.zip"
